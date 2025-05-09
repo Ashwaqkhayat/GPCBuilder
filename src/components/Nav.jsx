dd738b28f550873b547d55507de797c1d5f52c0e
Ashwaq Khayat <ashwaqkh@outlook.com>
2025-05-09 22:38:33 +0300

    Responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,39 @@
 import { List, X } from "@phosphor-icons/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Nav = () => {
+    const navRef = useRef();
 
-    // Handle the hamburger toggle
-    const [isListToggled, setListToggled] = useState(false);
-
-    function toggleHam() {
-        setListToggled(!isListToggled);
+    function showNav() {
+        navRef.current.classList.toggle("responsive_nav");
     }
 
     return (
         <header className="sm:px-16 px-8 py-8 absolute z-50 w-full">
-            <nav className="flex justify-between items-center max-container">
-                <a href="/" className="flex items-center space-x-3">
+            <div className="flex justify-center items-center max-container">
+                <a href="/" className="flex items-center space-x-3 mr-auto">
                     <img src="Logo.png" alt="Logo" />
                     <h1 className="hidden sm:block text-Mint font-lato font-bold text-xl">PC BUILDER</h1>
                 </a>
-                <div className="block sm:hidden">
-                    <List
-                        className={`cursor-pointer transition-all duration-300 ease-in-out ${isListToggled ? "hidden" : "block"}`}
-                        size={28} 
+                <nav ref={navRef} className="nav-slider">
+                    <a className="hover:text-Purple duration-500 nav-link" href="#">FAQs</a>
+                    <a className="hover:text-Purple duration-300 nav-link" href="#">Sign In</a>
+                    <X
+                        className="cursor-pointer nav-btn nav-close-btn"
+                        size={28}
                         weight="bold"
                         color="#DBF6E9"
-                        onClick={toggleHam}
+                        onClick={showNav}
                     />
-                    <X
-                        className={`cursor-pointer transition-all duration-300 ease-in-out ${isListToggled ? "block" : "hidden"}`}
-                        size={28}
-                        color="#dbf6e9"
-                        weight="bold"
-                        onClick={toggleHam}
-                    />
-                </div>
-                <ul className="hidden sm:flex font-lato text-sm justify-center items-center gap-7 sm:gap-16 text-Mint">
-                    <li><a className="hover:text-Purple duration-500" href="#">FAQs</a></li>
-                    <li><a className="hover:text-Purple duration-300" href="#">Sign In</a></li>
-                </ul>
-            </nav>
+                </nav>
+                <List
+                    className="cursor-pointer nav-btn nav-open-btn"
+                    size={28}
+                    weight="bold"
+                    color="#DBF6E9"
+                    onClick={showNav}
+                />
+            </div>
         </header>
     );
 }
